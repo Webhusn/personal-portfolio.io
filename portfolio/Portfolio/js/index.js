a5a76125c39033557323541c25131300f3dc3be6
Webhusn <96583400+Webhusn@users.noreply.github.com>
2025-01-05 00:09:14 +0530

    Add files via upload

---
+++ portfolio/Portfolio/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const navbar = document.querySelector('.navbar');
+   let lastScrollTop = 0;
+
+   window.addEventListener('scroll', () => {
+      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScroll > lastScrollTop) {
+
+         navbar.classList.add('navbar-hidden');
+      } else {
+
+         navbar.classList.remove('navbar-hidden');
+      }
+
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+   });
+});
